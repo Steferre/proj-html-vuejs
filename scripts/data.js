92b1bae04c43637a47344255ac6d62c592586d32
Steferre <ferrera.ste@gmail.com>
2021-05-25 16:56:13 +0200

    aggiunte liste footer con vue

---
+++ scripts/data.js
@@ -0,0 +1,45 @@
+const standardLinksList = [
+    {
+        text: "HOME",
+        active: false
+    },
+    {
+        text: "ABOUT",
+        active: false
+    },
+    {
+        text: "PROJECTS",
+        active: false
+    },
+    {
+        text: "SERVICES",
+        active: false
+    },
+    {
+        text: "BLOG",
+        active: false
+    },
+    {
+        text: "CONTACT",
+        active: false
+    },
+];
+
+const majorSocialList = [
+    {
+        social_name: "instagram",
+        class: "fa-instagram"
+    },
+    {
+        social_name: "linkedin",
+        class: "fa-linkedin"
+    },
+    {
+        social_name: "facebook",
+        class: "fa-facebook"
+    },
+    {
+        social_name: "twitter",
+        class: "fa-twitter"
+    }
+];
